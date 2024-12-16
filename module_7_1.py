70d975a24a4f71ac1f994cf8410514e52f2ba223
natashaseroff <natashaseroff@mail.ru>
2024-12-16 19:41:36 +0200

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -36,9 +36,10 @@ class Shop():
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
-p3 = Product('Potato', 50.5, 'Vegetables')
+p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)
+print(p2) # __str__
 
 s1.add(p1, p2, p3)
+
 print(s1.get_products())
